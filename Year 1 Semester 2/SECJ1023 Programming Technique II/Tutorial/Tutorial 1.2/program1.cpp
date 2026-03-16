243bda9f514dbbeecf730e72e13c49bb986f7b2d
Cheng Zhi Min <chengzhimin@graduate.utm.my>
2026-03-16 20:34:39 +0800

    Add files via upload

---
+++ Year 1 Semester 2/SECJ1023 Programming Technique II/Tutorial/Tutorial 1.2/program1.cpp
@@ -0,0 +1,44 @@
+// Programming Technique II (SCSJ1023)
+// Tutorial 1: Procedural Programming (PP) vs. Object-Oriented Programming (OOP)
+// Program 1: Using PP
+// Use the following coordinates: (1, 2) and (3, 4) for Circle 1 and 2 to test your code
+// The distance and slope between these two circle centres are 1.41 and 1 respectively
+
+#include <iostream>
+#include <iomanip>
+#include <cmath>
+using namespace std;
+
+// ! Task 1: Define a function to calculate the distance between two circle centres.
+double dis(int x1, int x2, int y1, int y2){
+	return sqrt(pow(x2-x1, 2) + pow(y2-y1, 2));
+}
+
+// ! Task 2: Define a function to calculate the slope between two circle centres.
+double slp(int x1, int x2, int y1, int y2){
+	return (y2-y1) / (x2-x1);
+}
+
+
+int main()
+{
+	int x1, y1; //centre coordinate for Circle 1
+	int x2, y2; //centre coordinate for Circle 2
+
+	// ! Task 3: Ask the user to enter the centre coordinates of two circles
+	cout << "Enter Circle 1 coordinate: ";
+	cin >> x1 >> x2;
+	cout << "Enter Circle 2 coordinate: ";
+	cin >> y1 >> y2;
+
+	// ! Task 4: Calculate the distance and slope between two circle centres
+	double distance = dis(x1, x2, y1, y2);
+	double slope = slp(x1, x2, y1, y2);
+
+	// ! Task 5: Display the distance and slope between two circle centres
+	cout << fixed << setprecision(2);
+	cout << "Distance between two circle centres: " << distance << endl;
+	cout << "Slope between two circle centres: " << slope << endl;
+
+	return 0;
+}
---
+++ Year 1 Semester 2/SECJ1023 Programming Technique II/Tutorial/Tutorial 1.2/program2.cpp
@@ -0,0 +1,46 @@
+// Programming Technique II (SCSJ1023)
+// Tutorial 1: Procedural Programming (PP) vs. Object-Oriented Programming (OOP)
+// Program 2: Using PP with struct
+// Use the following coordinates: (1, 2) and (3, 4) for Circle 1 and 2 to test your code
+// The distance and slope between these two circle centres are 1.41 and 1 respectively
+
+#include <iostream>
+#include <iomanip>
+#include <cmath>
+using namespace std;
+
+// ! Task 1: Define a struct data type to hold information about a circle
+struct Circle{
+	int a, b;
+};
+
+// ! Task 2: Define a function to calculate the distance between two circle centres.
+double dis(Circle c1, Circle c2){
+	return sqrt(pow(c1.b - c1.a, 2) + pow(c2.b - c2.a, 2));
+}
+
+// ! Task 3: Define a function to calculate the slope between two circle centres.
+double slp(Circle c1, Circle c2){
+	return (c2.b - c2.a) / (c1.b - c1.a);
+}
+
+int main()
+{
+	// ! Task 4: Define a circle with specified information from the struct data type
+	Circle c1, c2;
+	cout << "Enter Circle 1 coordinate: ";
+	cin >> c1.a >> c1.b;
+	cout << "Enter Circle 2 coordinate: ";
+	cin >> c2.a >> c2.b;
+
+	// ! Task 5: Calculate the distance and slope between two circle centres
+	double distance = dis(c1, c2);
+	double slope = slp(c1, c2);
+	
+	// ! Task 6: Display the distance and slope between two circle centres
+	cout << fixed << setprecision(2);
+	cout << "Distance between two circle centres: " << distance << endl;
+	cout << "Slope between two circle centres: " << slope << endl;
+
+	return 0;
+}
